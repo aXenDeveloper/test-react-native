a5f91967d3a2006b8231d4c98c4965f0936e3909
Maciej aXenDev Balcerzak <ithereplay@gmail.com>
2021-04-27 21:52:32 +0200

    Add test react-router-dom

--- App.tsx
+++ App.tsx
@@ -1,19 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView, Text, Platform } from 'react-native';
+import { Text, Platform, View } from 'react-native';
 import styled from 'styled-components/native';
+import { NativeRouter, Route, Link } from 'react-router-native';
+import Home from './views/Home';
+import About from './views/About';
 
 const StyledView = styled.SafeAreaView`
   flex: 1;
   background-color: #fff;
-  padding-top: ${Platform.OS === 'android' ? 25 : 0};
+  padding-top: ${Platform.OS === 'android' ? 25 : 0}px;
 `;
 
 export default function App() {
   return (
     <StyledView>
-      <Text>Open up App.tsx to start working on your apps!</Text>
-      <StatusBar style="auto" />
+      <NativeRouter>
+        <View>
+          <View>
+            <Link to="/" underlayColor="#f0f4f7">
+              <Text>Home</Text>
+            </Link>
+            <Link to="/about" underlayColor="#f0f4f7">
+              <Text>About</Text>
+            </Link>
+            <Link to="/topics" underlayColor="#f0f4f7">
+              <Text>Topics</Text>
+            </Link>
+          </View>
+
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </View>
+      </NativeRouter>
     </StyledView>
   );
 }
